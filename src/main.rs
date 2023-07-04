89e0e0524cb4439d92b869b51b89d6705ffc3f80
OlshaMB <michaelolshanskiy@icloud.com>
2023-07-04 15:26:07 +0200

    feat: Template for Create PopUp window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 slint::include_modules!();
 fn main() {
     let ui = MainWindow::new().unwrap();
+    ui.global::<State>().on_launch(|id| {
+        println!("id: {}", id);
+    });
     ui.run().unwrap();
 }
